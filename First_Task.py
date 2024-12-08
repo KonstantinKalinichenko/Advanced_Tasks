80acc62e4c403ce6644ee25f93a10204cac15879
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-08 11:06:56 +0200

    1) Added solution for the first task

--- First_Task.py
+++ First_Task.py
@@ -1,5 +1,13 @@
 def process_number(n):
-
+    summary = 0
+    for i in n:
+        summary += int(i)
+    if summary % 2 == 0 and summary * 2 <= 100:
+        return f'{summary * 2}'
+    elif summary * 3 <= 100:
+        return f'{summary * 3}'
+    else:
+        return 'Превышено'
 
 
 input_string = input()
