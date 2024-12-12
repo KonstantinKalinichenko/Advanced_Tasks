ded6b1fcfac652ae8ec2835ed9f7dc3b4e8d52ed
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-12 15:21:06 +0200

    Added Second_Task solution

--- Second_Task.py
+++ Second_Task.py
@@ -5,8 +5,23 @@ class Item:
         self.quantity = int(quantity)
 
 def generate_product_report(data):
-    """Ваш код"""
+    records = data.split(';')
+    items = [Item(*record.split(':')) for record in records]
+    full_report = {}
+    for item in items:
+        if item.product not in full_report:
+            full_report[item.product] = {}
+        if item.date not in full_report[item.product]:
+            full_report[item.product][item.date] = 0
+        full_report[item.product][item.date] += item.quantity
 
+    result = []
+    for product, dates in full_report.items():
+        product_report = f'{product}:'
+        for date, quantity in dates.items():
+            product_report += f' - {date}: {quantity}'
+        result.append(product_report)
+    return result
 
 
 input_data =  input()
