9aec9ee3bf5fe39a0545ce227b57bb32055d6294
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-17 23:09:27 +0200

    First short thoughts about task 3

--- Third_Task.py
+++ Third_Task.py
@@ -1,14 +1,26 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.courses = {}
-    """Ваш код"""
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+#         self.courses = {}
+#     """Ваш код"""
+#
+#
+# class CourseManager:
+#     def __init__(self):
+#         self.students = {}
+#     """Ваш код"""
+#
+#
+# students_info = input()
 
-class CourseManager:
-    def __init__(self):
-        self.students = {}
-    """Ваш код"""
 
-students_info = input()
 scores_info = input()
-"""Ваш код"""
+splitted_subjects = scores_info.split(';')
+print(splitted_subjects)
+
+subject_min = [i.split(',') for i in splitted_subjects]
+print(subject_min)
+subject_score = {}
+for i in subject_min:
+    subject_score[i[0]] = int(i[1])
+print(subject_score)
