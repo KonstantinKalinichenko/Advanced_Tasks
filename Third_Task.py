a9152da8ffe0075df9c2c9514a5be69cf699bee2
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-18 23:21:23 +0200

    Additional thoughts about class Student

--- Third_Task.py
+++ Third_Task.py
@@ -1,26 +1,57 @@
-# class Student:
-#     def __init__(self, name):
-#         self.name = name
-#         self.courses = {}
-#     """Ваш код"""
-#
-#
-# class CourseManager:
-#     def __init__(self):
-#         self.students = {}
-#     """Ваш код"""
-#
-#
-# students_info = input()
+class Student:
+    def __init__(self, name):
+        self.name = name # name of student
+        self.courses = {} # key - course, value - grade
+
+    def add_course(self, course_name, grade): # add new course and grade
+        self.courses[course_name] = grade
+
+    def get_grade(self, course_name): # get student's grade of chosen course
+        return self.courses[course_name]
+
+    def __repr__(self):
+        return f"Student(name = {self.name}, courses = {self.courses})"
 
 
-scores_info = input()
-splitted_subjects = scores_info.split(';')
-print(splitted_subjects)
+class CourseManager:
+    def __init__(self):
+        self.students = {} # тут будут пары Студент : Его балл по предмету
+    """Ваш код"""
 
-subject_min = [i.split(',') for i in splitted_subjects]
-print(subject_min)
-subject_score = {}
-for i in subject_min:
-    subject_score[i[0]] = int(i[1])
-print(subject_score)
+
+
+students_info = input()
+a = students_info.split(';')
+subjects = [i.split(',') for i in a]
+print(subjects)
+
+students = {}
+for subject in subjects:
+    name, course_name, grade = subject[0], subject[1], subject[2]
+    if name not in students:
+        students[name] = Student(name=name)
+    students[name].add_course(course_name, grade)
+
+
+# print(students["Анна"].get_grade("Психология"))
+
+def get_grade_for_student(students, student_name, course_name):
+    student = students.get(student_name)
+    return student.get_grade(course_name)
+
+print(get_grade_for_student(students, "Анна", "Психология"))
+
+
+
+
+
+
+# scores_info = input()
+#
+# splitted_subjects = scores_info.split(';')
+# subject_min = [i.split(',') for i in splitted_subjects]
+# subject_score = {}
+# for i in subject_min:
+#     subject_score[i[0]] = int(i[1])
+# print(subject_score)
+
